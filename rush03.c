5d52592265f61329d367381ca7c08a2b7656256a
omyzx <muizyasin001@gmail.com>
2026-06-14 09:45:47 +0800

    change to meet 25line criteria

--- rush03.c
+++ rush03.c
@@ -6,50 +6,48 @@
 /*   By: muhabin3 <muhabin3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 10:57:43 by muhabin3          #+#    #+#             */
-/*   Updated: 2026/06/13 17:35:54 by muhabin3         ###   ########.fr       */
+/*   Updated: 2026/06/14 09:44:58 by muhabin3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "rec.h"
 
+void	print_char(int x, int y, int i, int j);
+
 void	rush(int x, int y)
 {
-/*	int i, j;
-	for (i = 1; i <= x; i++)
+	int	i;
+	int	j;
+
+	j = 0;
+	while (j <= y)
 	{
-		for (j = 1; j <= y; j++)
+		i = 0;
+		while (i <= x)
 		{
-			if (i == 1 || i == x)
-				ft_putchar('x');
-			if(j == 1 || j == y)
-				ft_putchar('y');
-			else
-				ft_putchar(' ');
+			print_char(x, y, i, j);
+			i++;
 		}
 		ft_putchar('\n');
+		j++;
 	}
-*/
-
-	int i, j;
-
-	for(j=0; j<=y; j++)
-	{
-		for(i=0; i<=x; i++)
-		{
-			if(j == 0 || j == y)
-				if (i == 0)			
-					ft_putchar('A');
-				else if(i == x)
-					ft_putchar('C');
-				else
-					ft_putchar('B');
-			else
-				if(i == 0 || i == x)
-					ft_putchar('B');
-				else
-					ft_putchar(' ');
-		}
-		ft_putchar('\n');
-	}
-
 }
 
+void	print_char(int x, int y, int i, int j)
+{
+	if (j == 0 || j == y)
+	{
+		if (i == 0)
+			ft_putchar('A');
+		else if (i == x)
+			ft_putchar('C');
+		else
+			ft_putchar('B');
+	}
+	else
+	{
+		if (i == 0 || i == x)
+			ft_putchar('B');
+		else
+			ft_putchar(' ');
+	}
+}
