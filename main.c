f86703cc25b75ad24c701c3451443f6afe36b950
omyzx <muizyasin001@gmail.com>
2026-06-21 12:36:12 +0800

    update on parse

--- main.c
+++ main.c
@@ -6,20 +6,41 @@
 /*   By: muhabin3 <muhabin3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/21 11:14:16 by muhabin3          #+#    #+#             */
-/*   Updated: 2026/06/21 11:38:27 by muhabin3         ###   ########.fr       */
+/*   Updated: 2026/06/21 12:33:22 by muhabin3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 
 void	ft_putchar(char c);
 
-int	main()
+int	main(int argc, char **argv)
 {
 	int	grid[4][4];
 	int	i;
 	int	j;
 	char	x;
+	int	clues[16];
+
+	if (argc != 2)
+    		write(1, "Error\n", 6);
+	
+
+	i = 0;
+	j = 0;
+	while	(argv[1][i])
+	{
+		if(argv[1][i] == ' ')
+			i++;
+		else
+		{
+			clues[j] = atoi(&argv[1][i]);
+			j++;
+			while (argv[1][i] != ' ' && argv[1][i])
+				i++;
+		}
+	}
 
 	i = 0;
 	while(i < 4)
@@ -36,6 +57,7 @@ int	main()
 		i++;
 	}
 }
+
 
 void	ft_putchar(char c)
 {
