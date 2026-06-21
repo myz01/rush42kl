60636baa53a59c4056003b0507c36298316c0e00
Muhammad Muizuddin Bin Mohd Yasin <muhabin3@u82z06s04.42kl.edu.my>
2026-06-21 17:12:28 +0800

    remaining folder

---
+++ iyz/main.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhabin3 <muhabin3@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/21 11:14:16 by muhabin3          #+#    #+#             */
+/*   Updated: 2026/06/21 16:21:48 by muhabin3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c);
+
+int	main(int argc, char **argv)
+{
+	int	grid[4][4];
+	int	i;
+	int	j;
+	char	x;
+	int	clues[16];
+
+	if (argc != 2)
+    		write(1, "Error\n", 6);
+	
+
+	i = 0;
+	j = 0;
+	while	(argv[1][i])
+	{
+		if(argv[1][i] == ' ')
+			i++;
+		else
+		{
+			clues[j] = atoi(&argv[1][i]);
+			x = clues[j] + '0';
+			ft_putchar(x);
+			j++;
+			while (argv[1][i] != ' ' && argv[1][i])
+				i++;
+		}
+	}
+	ft_putchar('\n');
+
+	i = 0;
+	while(i < 4)
+	{	
+		j = 0;
+		while(j < 4)
+		{
+			grid[i][j] = 0;
+			x = grid[i][j] + '0';
+			ft_putchar(x);
+			j++;	
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+//trying to use this from main to this function 
+//void	ft_printgrid(int grid[4][4])
+//{
+//	int	row;
+//	int	col;
+	
+//	row = 0;
+//	while(i < 4)
+//	{
+//	
+//	}
+
+//}
---
+++ j/main.c
@@ -0,0 +1,122 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_printstr(char *str)
+{	
+	int	i;
+	
+	i = 0;
+	while(str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	ft_counstr(char *str)
+{
+	int	i;
+	
+	i = 0;
+	while(str[i])
+		i++;
+		
+	return (i);
+}
+
+int	is_digit(char c)
+{
+	if(c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+	i = 0;
+	
+	while(str[i])
+		i++;
+	return (i);
+}
+
+/* Count single digits only, ignore other digits, 0 means there would be double digits, else return single digit count*/
+int	ft_check_only_single_digit(char *str)
+{
+	int	i;
+	int	counter;
+	
+	i = 0;
+	counter = 0;
+	while(str[i] != '\0')
+	{
+		if(is_digit(str[i]))
+		{	
+			if(str[i] >= '5' && str[i] <= '9')
+				return (0);
+				
+			if(i == 0 && str[i+1] == ' ')
+			{
+				counter++;
+			}
+			else if(str[i + 1] == '\0' && str[i - 1] == ' ')
+			{
+				counter++;
+			}	
+			else if(str[i - 1] == ' ' && str[i + 1] == ' ')
+			{
+				counter++;
+			}
+			else
+				return (0);
+		}
+		
+		i++;
+	}
+	return (counter);
+}
+/*
+void	assign_sight_level(board, int row_index, int col_index)
+{	
+	int	number_of_sights;
+	
+	number_of_sights = 4;
+	while(str[i])
+	{
+		if(is_digit(str[i])
+		{
+			
+		}
+	}
+	
+	
+}*/
+
+int	main(int argc, char *argv[])
+{
+	if(argc != 2)
+	{
+		ft_printstr("Please input only 2 arguments. Example: ./a.out \"4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2\"\n");
+		return (0);
+	}
+	
+	if(ft_counstr(argv[1]) && !ft_check_only_single_digit(argv[1]))
+	{
+		ft_printstr("Please input only 16 single digits (1-4) to set the sight levels for 4 sides.");
+		return (0);
+	}
+	
+	/*char *user_sight_levels[];
+	
+	user_sight_levels = argv[1];
+	int	board[6][6] = {
+	
+	}*/
+}
