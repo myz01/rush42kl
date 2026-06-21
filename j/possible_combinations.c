68ec76cfe62b6efe989d8e41631ac3641adb180b
Jodene Hii <jodehii@u82z06s03.42kl.edu.my>
2026-06-21 16:54:46 +0800

    jodene

--- j/possible_combinations.c
+++
@@ -1,9 +0,0 @@
-/* dots */
-char	*one = "1:4";
-
-/* lines */
-char	*one_four = "14:1234";
-char	*one_two = "12:4213-4123";
-char	*one_three = "13:4312-4132-4231";
-char	*two_two = "22:2413-1423-3412";
-char	*two_three = "23:1432-2431-3421";
